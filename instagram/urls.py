25b160ccc89df8926f023f20ae758b6358dcb62e
goodnight <qjadjehd05@gmail.com>
2023-05-04 00:28:27 +0900

    third

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
+app_name = 'instagram'
+
 urlpatterns = [
-    path('', views.post_list)
+    path('', views.post_list),
+    path('<int:pk>/',views.post_detail),
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail), ( 정규 표현식, 위와 같은 경로 )
+    path('archives/<int:year>/', views.archives_year),
+    re_path(r'archives/(?P<year>20\d{2})/$', views.archives_year), # 20xx년도를 주소에 기입했을때만 반응하도록
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q','')
-    # request.POST
-    # request.FILES
+post_list = ListView.as_view(model=Post)
+# 밑의 post_list 함수에서 검색 기능만을 빼고 동일한 기능을 함
+# 장고에 내재된 클래스 기반 뷰
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q','')
+#     # request.POST
+#     # request.FILES
     
-    if q:
-        qs = qs.filter(message__icontains=q)
+#     if q:
+#         qs = qs.filter(message__icontains=q)
         
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q,
-    })
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q,
+#     })
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    response = HttpResponse()
+    response.write("Hello World!")
+    return response
+
+def archives_year(request, year):
+    return HttpResponse(f"{year}년 archives")
+    
